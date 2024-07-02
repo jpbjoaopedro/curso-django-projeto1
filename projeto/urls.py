c6e0a8421cf8a38ed06fbc8c9787958b7b151b40
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-07-02 16:06:01 -0300

    App criado e url mais organizado

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def my_view(request):
-    return HttpResponse("Opa, deu bom")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
 ]
---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Home")
+
+def sobre(request):
+    return HttpResponse("Sobre")
+
+def contato(request):
+    return HttpResponse("Contato")
