3d0a99fe49a33e58f2f6be40213751bc14fb14f4
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-07-02 21:18:24 -0300

    Templates e Renderização html

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
     return HttpResponse("Sobre")
