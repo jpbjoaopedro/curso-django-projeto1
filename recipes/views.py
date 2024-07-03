b6e7489eec079a435e2bcac889326a812642093d
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-07-03 10:08:33 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
-        'telefone': '(21) 98205-5397',
-    })
-
-def sobre(request):
-    return HttpResponse("Sobre")
