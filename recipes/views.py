dc5ccb80663eb160ffa59e72486b085d7a58f495
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-07-15 22:05:59 -0300

    Nome "core" corrigido e página individual da receita

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
-    return render(request, 'recipes/pages/home.html')
 
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
-        'telefone': '(21) 98205-5397',
-    })
-
-def sobre(request):
-    return HttpResponse("Sobre")
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
