7942fe8804ed27a8896e23a0d7889c19aecb76ca
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2024-07-22 14:26:40 -0300

    chore: Update recipe page layout and add preparation steps

--- recipes/views.py
+++ recipes/views.py
@@ -10,4 +10,5 @@ def home(request):
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context= {
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
